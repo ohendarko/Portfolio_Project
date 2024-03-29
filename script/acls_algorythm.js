9ef32639ddae205831c233e960831de8f082503f
ohendarko <ohendarko@gmail.com>
2024-03-29 17:45:54 +0000

    noticed a missing box

--- script/acls_algorythm.js
+++ script/acls_algorythm.js
@@ -223,6 +223,12 @@ let status_yes4 = false;
 let status_yes5 = false;
 let status_yes6 = false;
 let status_yes7 = false;
+let status_yes8 = false;
+let status_yes9 = false;
+let status_yes10 = false;
+let astatus_yes8 = false;
+let astatus_yes9 = false;
+let astatus_yes10 = false;
 document.querySelector('.nsr0-yes').addEventListener('click', () => {
   /*if (status_on) {
     console.log('yes');
@@ -295,13 +301,15 @@ document.querySelector('.nsr0-yes').addEventListener('click', () => {
     document.querySelector('.sr2-yes').addEventListener('click', () => {
       if (!status_yes3) {
         document.querySelector('.ar8').innerHTML = `↓`;
-        document.querySelector('.twelveb').innerHTML = `<p>CPR 2 min</p>
+        document.querySelector('.twelveb').innerHTML = `Shock`
+        document.querySelector('.sr4').innerHTML = `<p>CPR 2 min</p>
       <ul>
         <li><strong>Amiodarone</strong> or <strong>Lidocaine</strong></li>
         <li>Treat reversible causes</li>
       </ul>`;
         document.querySelector('.ar9').innerHTML = `↓`;
-        document.querySelector('.sr4').innerHTML = `<p>Rhythm Shockable?</p>
+        document.querySelector('.ar10').innerHTML = `↓`;
+        document.querySelector('.sr5').innerHTML = `<p>Rhythm Shockable?</p>
       <div style="display: flex; justify-content: center; align-items: center;">
         <button class="algorythm-button sr3-yes" style="margin-right: 20px;">Yes</button>
         <button class="algorythm-button sr3-no">No</button>
@@ -311,7 +319,9 @@ document.querySelector('.nsr0-yes').addEventListener('click', () => {
         document.querySelector('.ar8').innerHTML = ``;
         document.querySelector('.twelveb').innerHTML = ``;
         document.querySelector('.ar9').innerHTML = ``;
+        document.querySelector('.ar10').innerHTML = ``;
         document.querySelector('.sr4').innerHTML = ``;
+        document.querySelector('.sr5').innerHTML = ``;
         status_yes3 = false;
       }
 
@@ -379,5 +389,124 @@ document.querySelector('.nsr0-yes').addEventListener('click', () => {
       //document.querySelector('.ar7').innerHTML = ``;
       //document.querySelector('.sr2').innerHTML = ``;
     });
+
+    document.querySelector('.sr2-no').addEventListener('click', () => {
+      if (!astatus_yes8) {
+        document.querySelector('.ar8').innerHTML = `↓`;
+        document.querySelector('.twelveb').innerHTML = `<p> Are there signs on return of spontaneus circulation (ROSC)?</p>
+        <div style="display: flex; justify-content: center; align-items: center;">
+          <button class="algorythm-button ssr7-yes" style="margin-right: 20px;">Yes</button>
+          <button class="algorythm-button ssr7-no">No</button>
+        </div>`;
+        /*document.querySelector('.twelve').innerHTML = `<ul>
+        <li>If no signs on return of spontaneus circulation (ROSC), got to 10 or 11</li>
+        <li>If ROSC, go to post-cardiac arrest care</li>
+        <li>Consider appropriateness of continued rescuscitation</li>
+      </ul>`;
+      */
+        astatus_yes8 = true;
+      } else {
+        document.querySelector('.ar8').innerHTML = ``;
+        document.querySelector('.twelveb').innerHTML = ``;
+        astatus_yes8 = false;
+      }
+
+      document.querySelector('.ssr7-no').addEventListener('click', () => {
+        if (!astatus_yes9) {
+          document.querySelector('.ar9').innerHTML = `↓`;
+          document.querySelector('.sr4').innerHTML = `<p>CPR 2 min</p>
+          <ul>
+            <li>IV/IO Access</li>
+            <li><strong>Epinephrine</strong> 1mg every 3 to 5 mins</li>
+            <li>Consider advanced airway, capnography</li>
+          </ul>
+          <p><strong>OR</strong></p>
+          <ul>
+            <li>Treat reversible causes</li>
+          </ul>
+          <p>Consider appropriateness of continued rescuscitation</p>
+        </div>`;
+          astatus_yes9 = true;
+        } else {
+          document.querySelector('.ar9').innerHTML = ``;
+          document.querySelector('.sr4').innerHTML = ``;
+          astatus_yes9 = false;
+        }
+      });
+      document.querySelector('.ssr7-yes').addEventListener('click', () => {
+        if (!astatus_yes10) {
+          document.querySelector('.ar9').innerHTML = `↓`;
+          document.querySelector('.sr4').innerHTML = `<ul>
+          <li>Go to post-cardiac arrest care</li>
+        </ul>
+        <p>Consider appropriateness of continued rescuscitation</p>`;
+          astatus_yes10 = true;
+        } else {
+          document.querySelector('.ar9').innerHTML = ``;
+          document.querySelector('.sr4').innerHTML = ``;
+          astatus_yes10 = false;
+        }
+      });
+    });
   });
+
+  document.querySelector('.sr1-no').addEventListener('click', () => {
+    if (!status_yes8) {
+      document.querySelector('.ar5').innerHTML = `↓`;
+      document.querySelector('.fiveb').innerHTML = `<p> Are there signs on return of spontaneus circulation (ROSC)?</p>
+      <div style="display: flex; justify-content: center; align-items: center;">
+        <button class="algorythm-button sr7-yes" style="margin-right: 20px;">Yes</button>
+        <button class="algorythm-button sr7-no">No</button>
+      </div>`;
+      /*document.querySelector('.twelve').innerHTML = `<ul>
+      <li>If no signs on return of spontaneus circulation (ROSC), got to 10 or 11</li>
+      <li>If ROSC, go to post-cardiac arrest care</li>
+      <li>Consider appropriateness of continued rescuscitation</li>
+    </ul>`;
+    */
+      status_yes8 = true;
+    } else {
+      document.querySelector('.ar5').innerHTML = ``;
+      document.querySelector('.fiveb').innerHTML = ``;
+      status_yes8 = false;
+    }
+
+    document.querySelector('.sr7-no').addEventListener('click', () => {
+      if (!status_yes9) {
+        document.querySelector('.ar6').innerHTML = `↓`;
+        document.querySelector('.tenb').innerHTML = `<p>CPR 2 min</p>
+        <ul>
+          <li>IV/IO Access</li>
+          <li><strong>Epinephrine</strong> 1mg every 3 to 5 mins</li>
+          <li>Consider advanced airway, capnography</li>
+        </ul>
+        <p><strong>OR</strong></p>
+        <ul>
+          <li>Treat reversible causes</li>
+        </ul>
+        <p>Consider appropriateness of continued rescuscitation</p>
+      </div>`;
+        status_yes9 = true;
+      } else {
+        document.querySelector('.ar6').innerHTML = ``;
+        document.querySelector('.tenb').innerHTML = ``;
+        status_yes9 = false;
+      }
+    });
+    document.querySelector('.sr7-yes').addEventListener('click', () => {
+      if (!status_yes10) {
+        document.querySelector('.ar6').innerHTML = `↓`;
+        document.querySelector('.tenb').innerHTML = `<ul>
+        <li>Go to post-cardiac arrest care</li>
+      </ul>
+      <p>Consider appropriateness of continued rescuscitation</p>`;
+        status_yes10 = true;
+      } else {
+        document.querySelector('.ar6').innerHTML = ``;
+        document.querySelector('.tenb').innerHTML = ``;
+        status_yes10 = false;
+      }
+    });
+  });
+
 });
