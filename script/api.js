9cee87c1d26f67889340bd6fb848ece7c5eabfbc
ohendarko <ohendarko@gmail.com>
2024-04-03 07:07:29 +0000

    added security

--- script/api.js
+++ script/api.js
@@ -1,4 +1,4 @@
-const queryString = `https://newsapi.org/v2/everything?q=keyword&apiKey=27dddb94b7ba4cf8af8657fc2ac9ef12`;
+const queryString = `https://newsapi.org/v2/everything?q=keyword&
 
 
 request.get(queryString, (err, response, body) => {
