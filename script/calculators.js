7accc355f60fb660ff3c90904e014fa7522af065
ohendarko <ohendarko@gmail.com>
2024-04-05 00:13:41 +0000

    pushing final changes

--- script/calculators.js
+++ script/calculators.js
@@ -5,26 +5,26 @@ function calculatebmi() {
   let height = Number(inputElementHeight.value);
 
   if (isNaN(weight) || isNaN(height)) {
-    document.querySelector('.total-bmi').innerHTML = `Invalid input`;
     document.querySelector('.total-bmi').classList.add('red');
+    document.querySelector('.total-bmi').innerHTML = `Invalid input`;
     return;
   }
 
   if (weight === 0 && height === 0) {
-    document.querySelector('.total-bmi').innerHTML = `Weight missing. Input required. Height missing Input required`;
     document.querySelector('.total-bmi').classList.add('red');
+    document.querySelector('.total-bmi').innerHTML = `Weight missing; Input required. Height missing; Input required`;
     return;
   }
 
   if (weight === 0) {
-    document.querySelector('.total-bmi').innerHTML = `Weight missing. Input required`;
     document.querySelector('.total-bmi').classList.add('red');
+    document.querySelector('.total-bmi').innerHTML = `Weight missing; Input required`;
     return;
   }
 
   if (height === 0) {
-    document.querySelector('.total-bmi').innerHTML = `Height missing Input required`;
     document.querySelector('.total-bmi').classList.add('red');
+    document.querySelector('.total-bmi').innerHTML = `Height missing; Input required`;
     return;
   }
 
@@ -32,8 +32,12 @@ function calculatebmi() {
   let BMI = bmi.toFixed(2);
   //let BMI = inputElementHeight.value;
 
+  document.querySelector('.total-bmi').classList.add('white');
   document.querySelector('.total-bmi').innerHTML = `BMI = ${BMI} kg/sq.m`;
+
 }
+/*Fix colour changes*/
+/*Code for: Calculate as you type*/
 
 function handlebmiKeydown(event) {
   if (event.key === 'Enter') {
