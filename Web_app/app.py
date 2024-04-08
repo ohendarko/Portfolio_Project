c10a279219aac150a6dec324909635a1502a6814
ohendarko <ohendarko@gmail.com>
2024-04-08 21:18:16 +0000

    gitsend.sh ignore

--- Web_app/app.py
+++ Web_app/app.py
@@ -2,10 +2,12 @@ from flask import Flask, render_template, url_for, flash, redirect
 
 app = Flask(__name__)
 
+
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('home.html')
+
 
 @app.route("/acls-algorithm")
 def acls_algorithm():
