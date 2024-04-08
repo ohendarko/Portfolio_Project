b279249a2ccb15623e37a290c32abfc20b39852a
ohendarko <ohendarko@gmail.com>
2024-04-08 22:30:06 +0000

    flask implementation set. To run more tests later. To deploy tomorrow

--- Web_app/app.py
+++ Web_app/app.py
@@ -3,6 +3,11 @@ from flask import Flask, render_template, url_for, flash, redirect
 app = Flask(__name__)
 
 
+@app.route("/welcome")
+def welcome():
+    return render_template('landing_page.html')
+
+
 @app.route("/")
 @app.route("/home")
 def home():
