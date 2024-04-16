210ac398b6204b65d33a29a9512d0983d613958d
ohendarko <ohendarko@gmail.com>
2024-04-16 22:55:35 +0000

    updated init.py

--- Web_app/web_app/__init__.py
+++ Web_app/web_app/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+import email_validator
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
 
 from web_app import routes
