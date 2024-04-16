73ff588e6fceb65d2bb873100d09fc42981836b7
ohendarko <ohendarko@gmail.com>
2024-04-16 22:51:16 +0000

    added login and resiter routes: to set up pages soon

--- Web_app/web_app/routes.py
+++ Web_app/web_app/routes.py
@@ -37,3 +37,24 @@ def calculators():
 @app.route("/under_construction")
 def under_construction():
     return render_template('under_construction.html')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Register', form=form)
