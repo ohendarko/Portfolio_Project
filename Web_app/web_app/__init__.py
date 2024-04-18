06fafab68bb3ac0e2d99ca0539240d807a74871f
ohendarko <ohendarko@gmail.com>
2024-04-18 01:34:58 +0000

    local implementaion complete. Features are functional. To run more tests and edit designs later

--- Web_app/web_app/__init__.py
+++ Web_app/web_app/__init__.py
@@ -1,12 +1,17 @@
 from flask import Flask, url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import  Bcrypt
+from flask_login import LoginManager
 
 import email_validator
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SECRET_KEY'] = '0727f5ce927ba58e1709c9942adb5f5a9'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from web_app import routes
