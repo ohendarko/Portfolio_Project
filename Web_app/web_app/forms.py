a4c06b02c169ee7fa2ac3c37bf92c29ae8d6cd6a
ohendarko <ohendarko@gmail.com>
2024-04-18 22:18:15 +0000

    addded css configs for mobile view. To test later

--- Web_app/web_app/forms.py
+++ Web_app/web_app/forms.py
@@ -16,6 +16,16 @@ class RegistrationForm(FlaskForm):
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('This username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('This email is already in use.')
+
 
 class LoginForm(FlaskForm):
     email = StringField('Email',
